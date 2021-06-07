231472b8db83ad7a12d9f4e2447e7f0cf20f8057
Jon Yocky <6563138+Curlyfries13@users.noreply.github.com>
2021-06-07 08:12:47 -0700

    Add API required validation on SearchBox
    * add 100 character limit

--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState} from 'react';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 
 // This component is the search text box.
 // TODO: implement hook for ENTER
 const SearchBox = ({value, onUpdate} : Props) => {
+  const [err, setError] = useState(false)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
     // if we need validation set it up in this handler.
     if (event.target.value.length <= 100){
       onUpdate(event.target.value);
+      setError(false);
+    } else {
+      setError(true);
     }
-    // TODO: implement error state
   }
   return (
-    <FormControl>
+    <FormControl error={err}>
       <InputLabel htmlFor="search">Keyword</InputLabel>
       <Input
         id='search'
         type='text'
         onChange={handleChange}
+        aria-describedby="input-message"
       />
+      <FormHelperText id="input-message">
+        {err ? 'Search length is too long (must be less than 100 characters)' : ''}
+      </FormHelperText>
     </FormControl>
   )
 }
